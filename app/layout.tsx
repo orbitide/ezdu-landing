34a5d4e36a46a756f5ec50ece3973d37b7ba7642
hr-shuvo <h.r.shuvo9@gmail.com>
2026-01-09 02:32:53 +0600

    fix meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ReactNode } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,31 +16,53 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
     title: 'Online Learning Platform',
 
-    description: 'The best platform for online courses, tutorials, and practice.',
+    description: 'The best platform for learn and practice.',
     keywords: [
-        'online learning',
-        'courses',
-        'tutorials',
         'practice exams',
-        'education',
-        'e-learning',
         'study materials',
+        "SSC exam preparation",
+        "HSC model test",
+        "Class 6 MCQ practice",
+        "Class 7 exam",
+        "Class 8 questions",
+        "BCS preparation",
+        "Bank job preparation",
+        "Govt job preparation",
+        "SSC HSC previous questions",
+        "IELTS Preparation",
+        "IELTS Mock Test"
     ],
     icons: {
         icon: '/favicon.ico',
     },
     openGraph: {
-        title: 'Online Learning Platform',
-        description: 'The best platform for online courses, tutorials, and practice exams.',
+        title: 'Pocket Learning Companion',
+        description: 'The best platform for learn and practice.',
         url: 'https://ezdu.net',
         siteName: 'ezdu',
+        images: [
+            {
+                url: 'https://ezdu.net/logo.png',
+                width: 1200,
+                height: 630,
+                alt: 'Learning Platform',
+            },
+        ],
+        type: 'website',
     },
+    twitter:{
+        card: 'summary_large_image',
+        title: 'Pocket Learning Companion',
+        description: 'The best platform for learn and practice.',
+        images: ['https://ezdu.net/logo.png'],
+    },
+    metadataBase: new URL('https://ezdu.net'),
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
